0caf534943973d6e427b05fb19f9873f1ec473b7
Christopher <cvang13@madisoncollege.com>
2021-02-23 13:45:46 -0600

    moved test directory under edu.matc; added Dao tests (basic no real tests) into test directory

---
+++ src/main/java/edu/matc/test/EntryDaoTest.java
@@ -0,0 +1,36 @@
+package edu.matc.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntryDaoTest {
+
+    @Test
+    void getAllEntry() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void saveOrUpdate() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getByPropertyEqual() {
+    }
+
+    @Test
+    void getByPropertyLike() {
+    }
+}
