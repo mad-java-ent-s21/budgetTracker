1c0b888ee8a5acb7afd3ef85a123c1b06260418b
Christopher <cvang13@madisoncollege.com>
2021-04-11 21:12:54 -0500

    user test for generic dao all passes, haven't done last two tests

--- src/test/java/edu/matc/persistence/UserTest.java
+++ src/test/java/edu/matc/persistence/UserTest.java
@@ -2,7 +2,7 @@ package edu.matc.persistence;
 
 import edu.matc.entity.User;
 import edu.matc.testUtils.Database;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,12 +13,13 @@ class UserTest {
 
     GenericDao dao;
 
-    @Before
-    public void setUp() throws Exception {
-        dao = new GenericDao(User.class);
+    @BeforeEach
+    public void setUp() {
+
         Database database = Database.getInstance();
         database.runSQL("cleandb.sql");
 //        users = dao.getAll();
+        dao = new GenericDao(User.class);
     }
 
     @Test
@@ -41,14 +42,30 @@ class UserTest {
 
     @Test
     void delete() {
+        dao.delete(dao.getById(2));
+        assertNull(dao.getById(2));
     }
 
     @Test
     void insert() {
+        User newUser;
+        newUser = new User("NewGuy", "newguy", "New", "Guy", "newguy@some.com", "2000-02-16");
+        int id = dao.insert(newUser);
+        assertNotEquals(0,id);
+        User insertedUser = (User) dao.getById(id);
+        assertEquals(id, insertedUser.getId());
+        assertEquals("New", insertedUser.getFirstName());
     }
 
     @Test
     void saveOrUpdate() {
+        String newName = "Harry";
+
+        User userUpdate = (User) dao.getById(1);
+        userUpdate.setFirstName(newName);
+        dao.saveOrUpdate(userUpdate);
+        User retrieveUser = (User) dao.getById(1);
+        assertEquals(newName, retrieveUser.getFirstName());
     }
 
     @Test
